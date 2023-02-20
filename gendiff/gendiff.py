dbc07f0c1cc092c6017036ca081db8f8c7291cd9
Sergei Ezerskii <gaftezer@gmail.com>
2023-02-20 14:34:51 +0300

    Step 6

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -5,70 +5,92 @@ from gendiff.parsing import parsing_files
 
 def stylish(value, replacer=' ', spaces_count=4):
     def iter_(current_value, depth):
-        if not isinstance(current_value, dict):
-            return str(current_value)
+        #if not isinstance(current_value, list):
+            #return str(current_value)
 
         deep_indent_size = depth + spaces_count
         deep_indent = replacer * deep_indent_size
         current_indent = replacer * depth
         lines = []
-        for key, val in current_value.items():
-            if key[0] == '+':
+        for char in current_value:
+            if char['type'] == 'add':
                 new_deep = deep_indent[:-2]
                 action = '+ '
-                new_key = key[2:]
-            elif key[0] == '-':
+                lines.append(
+                    f'{new_deep}{action}{char["key"]}: {char["value"]}')
+            elif char['type'] == 'del':
                 new_deep = deep_indent[:-2]
                 action = '- '
-                new_key = key[2:]
-            else:
+                lines.append(
+                    f'{new_deep}{action}{char["key"]}: {char["value"]}')
+            elif char['type'] == 'no_changed':
                 new_deep = deep_indent
                 action = ''
-                new_key = key
-            lines.append(f'{new_deep}{action}{new_key}: {iter_(val, deep_indent_size)}')
+                lines.append(
+                    f'{new_deep}{action}{char["key"]}: {char["value"]}')
+            elif char['type'] == 'changed':
+                new_deep = deep_indent[:-2]
+                action = '- '
+                lines.append(
+                    f'{new_deep}{action}{char["key"]}: {char["value1"]}')
+                new_deep = deep_indent[:-2]
+                action = '+ '
+                lines.append(
+                    f'{new_deep}{action}{char["key"]}: {char["value2"]}')
+
+            elif char['type'] == 'root':
+                new_deep = deep_indent
+                action = ''
+                lines.append(
+                    f'{new_deep}{action}{char["key"]}: {iter_(char["value"], deep_indent_size)}')
+
         result = itertools.chain("{", lines, [current_indent + "}"])
         return '\n'.join(result)
 
     return iter_(value, 0)
 
 
-def bool_to_lower_case(dict_):
+def bool_to_lower_case(arr):
     """Convert True and False to lowercase string"""
 
     lower = {True: 'true', False: 'false', None: 'null'}
-    for k, v in dict_.items():
-        if isinstance(v, dict):
-            bool_to_lower_case(v)
-        else:
-            if v in lower:
-                dict_[k] = lower[v]
-    return dict_
+    for char in arr:
+        if isinstance(char, dict):
+            for k, v in char.items():
+                if isinstance(v, (list, dict)):
+                    bool_to_lower_case(v)
+                else:
+                    if v in lower:
+                        char[k] = lower[v]
+    return arr
 
 
 def generate_diff(first_file, second_file):
     file_1, file_2 = parsing_files(first_file, second_file)
 
     def diff_dict(dict1, dict2):
-        result = {}
+        result = []
         keys = sorted(set(list(dict1.keys()) + list(dict2.keys())))
         for key in keys:
             if key not in dict2:
-                result[f'- {key}'] = dict1[key]
+                result.append({'type': 'del', 'key': key, 'value': dict1[key]})
             elif key in dict1 and key in dict2:
-                if dict1[key] == dict2[key]:
-                    result[f'{key}'] = dict1[key]
+                if isinstance(dict1[key], dict) and isinstance(dict2[key],
+                                                               dict):
+                    result.append({'type': 'root', 'key': key, 'value': diff_dict(dict1[key], dict2[key])})
                 else:
-                    if isinstance(dict1[key], dict) and isinstance(dict2[key],
-                                                                   dict):
-                        result[key] = diff_dict(dict1[key], dict2[key])
+                    if dict1[key] == dict2[key]:
+                        result.append(
+                            {'type': 'no_changed', 'key': key,
+                             'value': dict1[key]})
                     else:
-                        result[f'- {key}'] = dict1[key]
-                        result[f'+ {key}'] = dict2[key]
+                        result.append({'type': 'changed', 'key': key,
+                                       'value1': dict1[key],
+                                       'value2': dict2[key]})
 
             else:
-                result[f'+ {key}'] = dict2[key]
+                result.append({'type': 'add', 'key': key, 'value': dict2[key]})
         return result
 
     return stylish(bool_to_lower_case(diff_dict(file_1, file_2)))
-# В таком виде выдаёт нужный результат. Теперь нужно разбить на
-# создание дифа и представление, но не сломать.
+
