c7c5a264ed02f5a4d752ad5e2f3a8224d8fc54e1
Sergei Ezerskii <gaftezer@gmail.com>
2023-03-28 16:54:00 +0300

    Step 8. Add test_argparse

--- gendiff/argparse.py
+++ gendiff/argparse.py
@@ -1,12 +1,12 @@
 import argparse
 
 
-def parse():
+def parse(argv=None):
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', dest='format', help='set format of output',
                         default='stylish')
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
     return args
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,7 +3,7 @@ import pytest
 from gendiff.gendiff import generate_diff
 from fixtures.fixture_gendiff import file1_json_flat, file2_json_flat, file1_yaml_flat, file2_yaml_flat
 from fixtures.fixture_gendiff import file1_json_nested, file2_json_nested, file1_yaml_nested, file2_yaml_nested
-
+from gendiff.argparse import parse
 
 @pytest.mark.parametrize(
     ('file_1', 'file_2'),
@@ -59,3 +59,10 @@ def test_format_json(file_1, file_2, formatter):
         expected = f.read()
 
     assert generate_diff(file_1, file_2, formatter) == expected
+
+
+def test_argparse():
+    args = parse(['file1', 'file2', '--format', 'plain'])
+    assert args.first_file == 'file1'
+    assert args.second_file == 'file2'
+    assert args.format == 'plain'
